a5b97fd5cb9fdcc823776a49979e8f25345a8483
jonghun.yoo <mmyjh86@gmail.com>
2018-04-14 14:51:22 +0900

    Bithumb Client - User와 Interaction하는 Class - In/Out Data의 무결성 검사를 해야 함
    Bithumb Core
    - Bithumb API Wrapping Class
    - Requst - Response를 전달만 함
    
    [TODO] Data를 어떻게 보여줄지 토론 해야 함.
     - 현재 방향은 DataFrame으로..

--- bithumb/client.py
+++ bithumb/client.py
@@ -1,137 +1,52 @@
-import base64
-import hashlib
-import hmac
-import time
-import urllib
-import requests
+from bithumb.core import *
+import pandas as pd
 
 
-class client:
+# Input Data 무결성 검사
+# Ouput Data reformatting을 해야 함
+
+class client :
     def __init__(self, conkey, seckey):
-        self.http = BithumbHttp(conkey, seckey)
+        self.api = RestApi(conkey, seckey)
 
-    def get_balance(self, **kwargs):
-        """
-        bithumb 거래소 회원 지갑 정보
-        :param currency: 코인 타입
-        :type currency: str
-        :return: json type
-        {
-            "status"    : "0000",
-            "data"      : {
-                "total_btc"     : "665.40127447",
-                "total_krw"     : "305507280",
-                "in_use_btc"    : "127.43629364",
-                "in_use_krw"    : "8839047.0000000000",
-                "available_btc" : "537.96498083",
-                "available_krw" : "294932685.000000000000",
-                "xcoin_last"    : "505000"
-            }
-        }        
-        """
-        return self.http.post('/info/balance', **kwargs)
+    def get_transactions(self, count=10):
+        r = self.api.recent_transactions(count=count)
+        return pd.DataFrame(r['data'])
 
-    def get_transactions(self, **kwargs):
-        """
-        거래소 거래 체결 완료 내역
-        :param coin: Coin 이름
-        :param count: 조회할 데이터의 개수
-        :return: json type
-        {
-            "status"    : 결과 상태 코드
-            "data"      : [
-                {
-                    "transaction_date"  : 거래 채결 시간
-                    "type"              : 판/구매 (ask, bid)
-                    "units_traded"      : 거래 Currency 수량
-                    "price"             : 1Currency 거래 금액
-                    "total"             : 총 거래금액
-                }...               
-            ]
-        }
-        """
-        coin = kwargs.get('coin', None)
-        if coin is None:
-            coin = "BTC"
-        else:
-            del (kwargs['coin'])
+    def get_balance(self, currency):
+        # out data format
+        #{'total_krw': 0, 'in_use_krw': 0, 'available_krw': 0, 'misu_krw': 0, 'total_dash': '0.00000000',
+        # 'in_use_dash': '0.00000000', 'available_dash': '0.00000000', 'misu_dash': 0, 'xcoin_last': '381000'}
+        # df = pd.DataFrame()
+        r = self.api.balance(currency=currency)
+        # 어떻게 보여줄 것인지..
+        return r['data']
 
-        uri = "/public/recent_transactions/" + coin
-        return self.http.get(uri, **kwargs)
+    def put_order(self, type, currency, price, unit):
+        r = self.api.place(type=type, price=price, units=unit, order_currency=currency)
+        return (type, currency, r['order_id'])
 
+    def get_order(self, order_desc):
+        r = self.api.orders(type=order_desc[0], currency=order_desc[1], order_id=order_desc[1])
+        return r
 
-class HttpMethod:
-    def __init__(self):
-        self.session = requests.session()
-
-    @property
-    def base_url(self):
-        return ""
-
-    def _handle_response(self, response):
-        """        
-        requests에 대한 error handling
-        """
-        # statue code가 000이 아닐경우, requests.exceptions.HTTPError 발생
-        # 이부분은 에러처리를 어떻게 할 것인지 논의를 더 해 봐야 함
-        # response.raise_for_status()
-        return response.json()
-
-    def update_headers(self, headers):
-        self.session.headers.update(headers)
-
-    def post(self, path, timeout=1, **kwargs):
-        uri = self.base_url + path
-        response = self.session.post(url=uri, data=kwargs, timeout=timeout)
-        return self._handle_response(response)
-
-    def get(self, path, timeout=1, **kwargs):
-        uri = self.base_url + path
-        response = self.session.get(url=uri, data=kwargs, timeout=timeout)
-        return self._handle_response(response)
-
-
-class BithumbHttp(HttpMethod):
-    def __init__(self, conkey, seckey):
-        self.API_CONKEY = conkey.encode('utf-8')
-        self.API_SECRET = seckey.encode('utf-8')
-        super(BithumbHttp, self).__init__()
-
-    @property
-    def base_url(self):
-        return "https://api.bithumb.com"
-
-    def _signature(self, path, nonce, **kwargs):
-        query_string = path + chr(0) + urllib.parse.urlencode(kwargs) + chr(0) + nonce
-        h = hmac.new(self.API_SECRET, query_string.encode('utf-8'), hashlib.sha512)
-        return base64.b64encode(h.hexdigest().encode('utf-8'))
-
-    def post(self, path, **kwargs):
-        kwargs['endpoint'] = path
-        nonce = str(int(time.time() * 1000))
-
-        self.update_headers({
-            'Api-Key': self.API_CONKEY,
-            'Api-Sign': self._signature(path, nonce, **kwargs),
-            'Api-Nonce': nonce
-        })
-        return super().post(path, **kwargs)
+    def put_cancel(self, order_desc):
+        r = self.api.cancel(type=order_desc[0], currency=order_desc[1], order_id=order_desc[1])
+        return r
 
 
 if __name__ == "__main__":
-    bithumb = client('', '')
+    c = client('CONKEY', 'SECKEY')
 
-    # 잔고 조회
-    response = bithumb.get_balance(currency="ETH")
-    print(response)
+    r = c.get_transactions()
+    print (r)
 
-    # 최근 거래 완료 내역
-    response = bithumb.get_transactions(count=4)
-    for p in response['data']:
-        print ("최근 체결가:", p['price'])
-    #
-    # response = bithumb.get_transactions()
-    # print("최근 체결가:", response['data'][0]['price'])
-    #
-    # response = bithumb.get_transactions(coin="ETH")
-    # print("최근 체결가:", response['data'][0]['price'])
+    # r = c.get_balance("DASH")
+    # print (r)
+
+    # info = c.put_order("ask", "XRP", 1000, 1000)
+    # print (info)
+    # r = c.get_order(info)
+    # print (r)
+    # r = c.put_cancel(info)
+    # print(response)
---
+++ bithumb/core.py
@@ -0,0 +1,155 @@
+import base64
+import hashlib
+import hmac
+import time
+import urllib
+import requests
+
+
+class RestApi:
+    def __init__(self, conkey, seckey):
+        self.http = BithumbHttp(conkey, seckey)
+
+    def balance(self, **kwargs):
+        """
+        bithumb 거래소 회원 지갑 정보
+        :param currency: 코인 타입        
+        :return: json type
+        {
+            "status"    : "0000",
+            "data"      : {
+                "total_btc"     : "665.40127447",
+                "total_krw"     : "305507280",
+                "in_use_btc"    : "127.43629364",
+                "in_use_krw"    : "8839047.0000000000",
+                "available_btc" : "537.96498083",
+                "available_krw" : "294932685.000000000000",
+                "xcoin_last"    : "505000"
+            }
+        }        
+        """
+        return self.http.post('/info/balance', **kwargs)
+
+    def recent_transactions(self, **kwargs):
+        """
+        거래소 거래 체결 완료 내역
+        :param coin: Coin 이름
+        :param count: 조회할 데이터의 개수
+        :return: json type
+        {
+            "status"    : 결과 상태 코드
+            "data"      : [
+                {
+                    "transaction_date"  : 거래 채결 시간
+                    "type"              : 판/구매 (ask, bid)
+                    "units_traded"      : 거래 Currency 수량
+                    "price"             : 1Currency 거래 금액
+                    "total"             : 총 거래금액
+                }...               
+            ]
+        }
+        """
+        coin = kwargs.get('coin', None)
+        if coin is None:
+            coin = "BTC"
+        else:
+            del (kwargs['coin'])
+
+        uri = "/public/recent_transactions/" + coin
+        print(kwargs)
+        return self.http.get(uri, **kwargs)
+
+    def place(self, **kwargs):
+        """    
+        매수/매도 주문을 발행한다
+        :param type: 구매/판매 구분자로 "bid", "ask" 중 하나의 값을 갖는다
+        :param currency: Bithumb의 코인 타입 
+        :param unit: 수량
+        :param price: 단가         
+        :return: json type
+        {
+            "status"    : "0000",
+            "order_id"  : "1428646963419",
+            "data": [
+                {
+                    "cont_id"   : "15313",
+                    "units"     : "0.61460000",
+                    "price"     : "284000",
+                    "total"     : 174546,
+                    "fee"       : "0.00061460"
+                },
+                {
+                    "cont_id"   : "15314",
+                    "units"     : "0.18540000",
+                    "price"     : "289000",
+                    "total"     : 53581,
+                    "fee"       : "0.00018540"
+                }
+            ]
+        }
+        """
+        return self.http.post('/trade/place', **kwargs)
+
+    def orders(self, **kwargs):
+        return self.http.post('/info/orders', **kwargs)
+
+    def cancel(self, **kwargs):
+        return self.http.post('/trade/cancel', **kwargs)
+
+
+class HttpMethod:
+    def __init__(self):
+        self.session = requests.session()
+
+    @property
+    def base_url(self):
+        return ""
+
+    def _handle_response(self, response):
+        """        
+        requests에 대한 error handling
+        """
+        # statue code가 000이 아닐경우, requests.exceptions.HTTPError 발생
+        # 이부분은 에러처리를 어떻게 할 것인지 논의를 더 해 봐야 함
+        # response.raise_for_status()
+        return response.json()
+
+    def update_headers(self, headers):
+        self.session.headers.update(headers)
+
+    def post(self, path, timeout=1, **kwargs):
+        uri = self.base_url + path
+        response = self.session.post(url=uri, data=kwargs, timeout=timeout)
+        return self._handle_response(response)
+
+    def get(self, path, timeout=1, **kwargs):
+        uri = self.base_url + path
+        response = self.session.get(url=uri, params=kwargs, timeout=timeout)
+        return self._handle_response(response)
+
+
+class BithumbHttp(HttpMethod):
+    def __init__(self, conkey, seckey):
+        self.API_CONKEY = conkey.encode('utf-8')
+        self.API_SECRET = seckey.encode('utf-8')
+        super(BithumbHttp, self).__init__()
+
+    @property
+    def base_url(self):
+        return "https://api.bithumb.com"
+
+    def _signature(self, path, nonce, **kwargs):
+        query_string = path + chr(0) + urllib.parse.urlencode(kwargs) + chr(0) + nonce
+        h = hmac.new(self.API_SECRET, query_string.encode('utf-8'), hashlib.sha512)
+        return base64.b64encode(h.hexdigest().encode('utf-8'))
+
+    def post(self, path, **kwargs):
+        kwargs['endpoint'] = path
+        nonce = str(int(time.time() * 1000))
+
+        self.update_headers({
+            'Api-Key': self.API_CONKEY,
+            'Api-Sign': self._signature(path, nonce, **kwargs),
+            'Api-Nonce': nonce
+        })
+        return super().post(path, **kwargs)
