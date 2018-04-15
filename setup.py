75db1bb64dc43ebf154d413f00541bdbbec1938b
jonghun.yoo <mmyjh86@gmail.com>
2018-04-15 10:25:12 +0900

    Release python package v.0.0.1 - >> pip install pybithumb - https://pypi.python.org/pypi?%3Aaction=pkg_edit&name=pybithumb

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url             = 'https://github.com/sharebook/pybithumb',
     author          = 'Lukas Yoo, Brayden Jo',
     author_email    = 'mmyjh86@gmail.com, pystock@outlook.com',
+    install_requires=['requests', 'pandas'],
     license         = 'MIT',
     packages        = ['pybithumb'],
     zip_safe        = False
