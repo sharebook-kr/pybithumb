2b61433d71e6e6aa3a9a2ae0d570790c6565a114
brayden-jo <brayden.jo@outlook.com>
2018-11-06 16:19:23 +0900

    차트 API 업데이트
    bithumb 소스 변경에 따라 str을 float로 변경한 후 저장

--- pybithumb/history.py
+++ pybithumb/history.py
@@ -2,10 +2,25 @@ import requests
 import datetime
 import pandas as pd
 
+
 def get_ohlcv(symbol="BTC", interval="day"):
     try:
-        if interval == "hour":
+        if interval == "minute":
+            url = "https://www.bithumb.com/resources/chart/{}_xcoinTrade_01M.json".format(symbol)
+        elif interval == "minute3":
+            url = "https://www.bithumb.com/resources/chart/{}_xcoinTrade_03M.json".format(symbol)
+        elif interval == "minute5":
+            url = "https://www.bithumb.com/resources/chart/{}_xcoinTrade_05M.json".format(symbol)
+        elif interval == "minute10":
+            url = "https://www.bithumb.com/resources/chart/{}_xcoinTrade_10M.json".format(symbol)
+        elif interval == "minute30":
+            url = "https://www.bithumb.com/resources/chart/{}_xcoinTrade_30M.json".format(symbol)
+        elif interval == "hour":
             url = "https://www.bithumb.com/resources/chart/{}_xcoinTrade_01H.json".format(symbol)
+        elif interval == "hour6":
+            url = "https://www.bithumb.com/resources/chart/{}_xcoinTrade_06H.json".format(symbol)
+        elif interval == "hour12":
+            url = "https://www.bithumb.com/resources/chart/{}_xcoinTrade_12H.json".format(symbol)
         else:
             url = "https://www.bithumb.com/resources/chart/{}_xcoinTrade_24H.json".format(symbol)
 
@@ -18,7 +33,7 @@ def get_ohlcv(symbol="BTC", interval="day"):
             timestamp = x[0]
             date = datetime.datetime.fromtimestamp(timestamp/1000)
             date_list.append(date)
-            ohlcv = {'open': x[1], 'high': x[3], 'low': x[4], 'close': x[2], 'volume': x[5]}
+            ohlcv = {'open': float(x[1]), 'high': float(x[3]), 'low': float(x[4]), 'close': float(x[2]), 'volume': float(x[5])}
             ohlcv_list.append(ohlcv)
 
         df = pd.DataFrame(ohlcv_list, columns=['open', 'high', 'low', 'close', 'volume'], index=date_list)
@@ -28,5 +43,5 @@ def get_ohlcv(symbol="BTC", interval="day"):
 
 
 if __name__ == "__main__":
-    #print(get_ohlcv("BTC"))
-    print(get_ohlcv("BTC", interval="hour"))
+    df = get_ohlcv("BTC", interval="minute5")
+    print(df.head())
