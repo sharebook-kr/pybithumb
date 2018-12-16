dea06385a7385d4bd566aeb99a092870369cc40f
brayden-jo <brayden.jo@outlook.com>
2018-12-16 22:23:40 +0900

    버전 증가

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name            = 'pybithumb',
-    version         = '0.1.27',
+    version         = '0.1.28',
     description     = 'python wrapper for Bithumb API',
     url             = 'https://github.com/sharebook-kr/pybithumb',
     author          = 'Lukas Yoo, Brayden Jo',
