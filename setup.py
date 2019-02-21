04a725361fa1dfeeec7252a2b6c38f3c8c208459
jonghun.yoo <jonghun.yoo@outlook.com>
2019-02-21 21:57:04 +0900

    upgrade numpy v16.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name            = 'pybithumb',
-    version         = '0.1.28',
+    version         = '0.1.29',
     description     = 'python wrapper for Bithumb API',
     url             = 'https://github.com/sharebook-kr/pybithumb',
     author          = 'Lukas Yoo, Brayden Jo',
