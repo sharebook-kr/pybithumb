286336cc2307b64142d8e0ebd3db27171afd6e0d
Brayden <brayden.jo@outlook.com>
2022-02-19 07:59:21 +0900

    websocket version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url             = 'https://github.com/sharebook-kr/pybithumb',
     author          = 'Lukas Yoo, Brayden Jo',
     author_email    = 'jonghun.yoo@outlook.com, pystock@outlook.com',
-    install_requires=['requests', 'pandas', 'bs4', 'html5lib', 'datetime', 'websockets'],
+    install_requires=['requests', 'pandas', 'bs4', 'html5lib', 'datetime', 'websockets==9.1'],
     license         = 'MIT',
     packages        = ['pybithumb'],
     zip_safe        = False
